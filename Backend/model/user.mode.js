54d05f5940b3df59e45ea7f2a6b5130117168b70
arko1995 <shahriarsohelarko@gmail.com>
2026-08-03 16:12:06 +0600

    created user model

---
+++ Backend/model/user.mode.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["teacher", "admin", "student"],
+      required: true,
+    },
+    course: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async () => {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
