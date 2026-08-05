1fafe978eabb740c29bbf434cb1a4bac02f7ba90
arko1995 <shahriarsohelarko@gmail.com>
2026-08-05 23:56:21 +0600

    fixing bugs

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -8,7 +8,7 @@ const protectRoute = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader && !authHeader.startsWith("Bearer ")) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({
         success: false,
         message: "Authentication required",
@@ -32,9 +32,9 @@ const protectRoute = async (req, res, next) => {
 
     next();
   } catch (error) {
-    res.status(500).json({
+    res.status(401).json({
       success: false,
-      message: "Internal server error",
+      message: "Invalid or expired token",
       error: error,
     });
   }
@@ -42,7 +42,7 @@ const protectRoute = async (req, res, next) => {
 
 const allowRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req.user || !allowRoles.includes(req.user.role)) {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
       return res.status(403).json({
         success: false,
         message: "Permission denied",
