010683d2de17f9865f2f432d8905873eb239e205
arko1995 <shahriarsohelarko@gmail.com>
2026-08-06 16:59:16 +0600

    fixing routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import loginRouter from "./routes/auth.route.js";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", userRouter);
 app.use("/api/auth", loginRouter);
 
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { createUser, getUser } from "../controller/user.controller.js";
+import {
+  createUser,
+  getUser,
+  updateUser,
+} from "../controller/user.controller.js";
+import { allowRoles, protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.get("/getuser", getUser);
-router.post("/createuser", createUser);
+router.use(protectRoute);
+router.use(allowRoles("admin"));
+router.route("/").get(getUser).post(createUser);
+router.patch("/:id", updateUser);
 
 export default router;
