707a7cc1f24dd6ff3866151491a978848125a6f0
arko1995 <shahriarsohelarko@gmail.com>
2026-08-09 17:37:24 +0600

    submission controller

--- Backend/src/routes/submission.route.js
+++ Backend/src/routes/submission.route.js
@@ -6,9 +6,8 @@ import {
 import { protectRoute, allowRoles } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.use(protectRoute);
-router
-  .route("/:assignmentId")
-  .post(allowRoles("student"), createSubmission)
-  .patch(allowRoles("student"), updateSubmission);
+router.route("/:assignmentId").post(allowRoles("student"), createSubmission);
+
+router.route("/:submissionId").patch(allowRoles("student"), updateSubmission);
 
 export default router;
