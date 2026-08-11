cfbc4c46b4848d6f231bd06354dd19e4af4f1cd6
arko1995 <shahriarsohelarko@gmail.com>
2026-08-12 00:10:07 +0600

    adding react router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Admin from "./pages/Admin.jsx";
+import Student from "./pages/Student.jsx";
+import Teacher from "./pages/Teacher.jsx";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/teacher" element={<Teacher />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
