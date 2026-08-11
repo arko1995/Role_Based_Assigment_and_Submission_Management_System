8ebf2aec8532f89b2b4831838924d4030823d55c
arko1995 <shahriarsohelarko@gmail.com>
2026-08-11 16:04:11 +0600

    assignment state

--- Frontend/src/store/assignmentStore.js
+++ Frontend/src/store/assignmentStore.js
@@ -25,11 +25,11 @@ export const useAssignmentStore = create((set) => {
     }
   };
 
-  getAssignment: (id) => {
+  getAssignment: async (id) => {
     try {
       set({ loading: true, error: null });
 
-      const response = api.get(`/assignment/${id}`);
+      const response = await api.get(`/assignment/${id}`);
 
       set({
         selectedAssignment: response.data,
@@ -39,6 +39,91 @@ export const useAssignmentStore = create((set) => {
       set({
         loading: false,
         error: error.response?.data?.message || "Could not load assignment",
+      });
+    }
+  };
+
+  createAssignment: async (data) => {
+    try {
+      set({ loading: true, error: null });
+      const response = await api.post("/assignment", data);
+
+      set((state) => {
+        assignments = [...state.assignments, response.data];
+      });
+      set({ loading: false });
+      return response.data;
+    } catch (error) {
+      set({
+        loading: false,
+        error: error.response?.data?.message || "Assignment creation failed",
+      });
+    }
+  };
+
+  updateAssignment: async (id, data) => {
+    try {
+      set({ loading: true, error: null });
+
+      const response = api.patch(`/assignment/${id}`, data);
+
+      set((state) => {
+        assignments: state.assignments.map((assignment) =>
+          assignment.id === id ? response.data : assignment,
+        );
+      });
+
+      set({ loading: false });
+
+      return response.data;
+    } catch (error) {
+      set({
+        loading: false,
+        error: error.response?.data?.message || "Failed to update assignment",
+      });
+    }
+  };
+
+  publishAssignment: async (id) => {
+    try {
+      set({ loading: false, error: null });
+
+      const response = await api.patch(`/assignment/${id}/publish`);
+
+      set((state) => {
+        assignments: state.assignments.map((assignment) =>
+          assignment.id === id ? response.data : assignment,
+        );
+      });
+
+      set({ loading: false });
+
+      return response.data;
+    } catch (error) {
+      set({
+        loading: false,
+        error: error.response?.data?.message || "Failed publishing assignment",
+      });
+    }
+  };
+
+  deleteAssignment: async (id) => {
+    try {
+      set({ loading: false, error: null });
+
+      const response = await api.delete(`/assignment/${id}`);
+
+      set((state) => {
+        assignments: state.assignments.filter(
+          (assignment) => assignment.id !== id,
+        );
+      });
+
+      set({ loading: false });
+    } catch (error) {
+      set({
+        loading: false,
+        error: error.response?.data?.message || "Failed to delete assignment",
       });
     }
   };
