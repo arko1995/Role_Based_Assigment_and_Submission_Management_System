f22b9f6975ee323855d691c8fa4841fa84824518
arko1995 <shahriarsohelarko@gmail.com>
2026-08-12 20:14:03 +0600

    protectroute component

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuthStore } from "../store/authStore";
 
-const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const user = useAuthStore((state) => state.user);
+
+  const Navigate = useNavigate();
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!allowedRoles.includes(user.role)) {
+    return <Navigate to={`/${user.role}`} replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
