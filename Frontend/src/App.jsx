a2d538f17bfcd1620bdcb0da78d81e31cbf6bbbf
arko1995 <shahriarsohelarko@gmail.com>
2026-08-13 00:54:32 +0600

    adding protected route to app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,36 @@ import Login from "./pages/Login.jsx";
 import Admin from "./pages/Admin.jsx";
 import Student from "./pages/Student.jsx";
 import Teacher from "./pages/Teacher.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/student" element={<Student />} />
-        <Route path="/teacher" element={<Teacher />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={["admin"]}>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/student"
+          element={
+            <ProtectedRoute allowedRoles={["student"]}>
+              <Student />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/teacher"
+          element={
+            <ProtectedRoute allowedRoles={["teacher"]}>
+              <Teacher />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
